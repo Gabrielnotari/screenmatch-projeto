9e1cf2892b61457d4d2661154e624bac8d363d46
gabriel <gabriel.notari@icloud.com>
2025-04-30 15:47:57 -0300

    Adição de métodos

--- projetoalura/src/main/java/br/com/alura/projetoalura/model/Serie.java
+++ projetoalura/src/main/java/br/com/alura/projetoalura/model/Serie.java
@@ -27,7 +27,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @Transient
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie(){}
@@ -55,6 +55,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -123,6 +124,7 @@ public class Serie {
                         ", avaliacao=" + avaliacao +
                         ", atores='" + atores + '\'' +
                         ", poster='" + poster + '\'' +
-                        ", sinopse='" + sinopse + '\'';
+                        ", sinopse='" + sinopse + '\''+
+                        ", episodios='" + episodios + '\'';
     }
 }
